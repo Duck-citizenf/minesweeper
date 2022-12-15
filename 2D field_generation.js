e3f099440aa04623397373dbfde44c8430563885
Duck-citizenf <81924621+Duck-citizenf@users.noreply.github.com>
2022-12-15 17:23:35 +0300

    Add files via upload

--- 2D field_generation.js
+++ 2D field_generation.js
@@ -29,7 +29,7 @@
     const div_ = "</div>";
     let x = 0;
     let y = 0;
-    let number = 0;
+    let bombs = 0;
     DigField.forEach((array) => array.forEach((element) =>
         {
             let FS = DigField[y+1];         // ________
@@ -39,36 +39,46 @@
                                             //|__|__|__|
                                             //|SW|S |SE|
                                             //|__|__|__|
-            
+            //Creating cell with a bomb
             if(element == 1) {field.innerHTML += "<div id=\"cell\" class=\"bomb untaken\"></div>"}
+            //Creating other cells and counting how many bombs are nearby
             else {
+                //Testing if north exist
                 if (FN != undefined){
                     let NW = DigField[y-1][x-1];
                     let N  = DigField[y-1][x];  
                     let NE = DigField[y-1][x+1];
-                    if (NE != undefined){number += NE};
-                    if (N  != undefined){number += N };
-                    if (NW != undefined){number += NW};
+                    //Testing if north-west or north-east exist
+                    if (NE != undefined){bombs += NE};
+                    if (NW != undefined){bombs += NW};
+                    bombs += N;
                 };
+                //Testing if south exist
                 if (FS != undefined){
                     let SW = DigField[y+1][x-1];
                     let S  = DigField[y+1][x];  
                     let SE = DigField[y+1][x+1];
-                    if (SE != undefined){number += SE};
-                    if (S  != undefined){number += S };
-                    if (SW != undefined){number += SW};
+                    //Testing if south-west or south-east exist
+                    if (SE != undefined){bombs += SE};
+                    if (SW != undefined){bombs += SW};
+                    bombs += S;
                 };
-                if (E  != undefined){number += E };
-                if (W  != undefined){number += W };
+                //Testing if west or east exist
+                if (E  != undefined){bombs += E };
+                if (W  != undefined){bombs += W };
 
-                if(number > 0) {
-                    field.innerHTML+=_div+number+div_
+                //If we have even 1 bomb with write it in HTML
+                if(bombs > 0) {
+                    field.innerHTML+=_div+bombs+div_
                 } 
+                //Otherwise cell will be blank
                 else {
                     field.innerHTML+=zero_cell
                 }
             };
-            number = 0;
+            //Setting count to 0 for the new cicle
+            bombs = 0;
+            //Changing coordinates
             x = x == 29 ? 0 : x+1;
             y = x == 0 ? y+1 : y;
         }
--- 2D mechanics.js
+++ 2D mechanics.js
@@ -1,82 +1,144 @@
 (function() {
     const allcells = document.querySelectorAll("#cell");
-    const DigAllCells = [];
+    //Converting NodeList of Listeners to Array
     const allcells2 = Array.from(allcells);
+    //Converting Array to 
+    const DigAllCells = [];
     while(allcells2.length) DigAllCells.push(allcells2.splice(0,30));
 
     DigAllCells.forEach((m) => m.forEach((i) =>{
+            //blocking left click in browser
             i.oncontextmenu = function () {
                 return false;
             };
             i.addEventListener("click", function() {  
                 let flag = 0; 
+                //Taking coordinates of Listener
                 let y = DigAllCells.indexOf(m);
                 let x = m.indexOf(i);
+                //FS - south array, FN - north array
                 let FS = DigAllCells[y+1];   
-                let FN = DigAllCells[y-1];   
+                let FN = DigAllCells[y-1];
+                //West and East from Listener
                 let W  = DigAllCells[y][x-1];
                 let E  = DigAllCells[y][x+1];
+                //Testing if north exist
                 if (FN != undefined){
-                    let NW = DigAllCells[y-1][x-1];
-                    let N  = DigAllCells[y-1][x];  
-                    let NE = DigAllCells[y-1][x+1];
+                    window.NW = DigAllCells[y-1][x-1];
+                    window.N  = DigAllCells[y-1][x];  
+                    window.NE = DigAllCells[y-1][x+1];
+                    //testing whether they have flag or not, if north-west or north-east exist
                     if(NW != undefined){NW.classList.contains('flag') == true ? flag++ : null}
-                    if(N  != undefined){N .classList.contains('flag') == true ? flag++ : null}
                     if(NE != undefined){NE.classList.contains('flag') == true ? flag++ : null}
+                    N .classList.contains('flag') == true ? flag++ : null
                 };
+                //Testing if south exist
                 if (FS != undefined){
-                    let SW = DigAllCells[y+1][x-1];
-                    let S  = DigAllCells[y+1][x];  
-                    let SE = DigAllCells[y+1][x+1];
+                    window.SW = DigAllCells[y+1][x-1];
+                    window.S  = DigAllCells[y+1][x];  
+                    window.SE = DigAllCells[y+1][x+1];
+                    //testing whether they have flag or not, if south-west or south-east exist
                     if(SW != undefined){SW.classList.contains('flag') == true ? flag++ : null}
-                    if(S  != undefined){S .classList.contains('flag') == true ? flag++ : null}
                     if(SE != undefined){SE.classList.contains('flag') == true ? flag++ : null}
+                    S .classList.contains('flag') == true ? flag++ : null
                 };
+                //testing whether they have flag or not, whether west or east exist
                 if(W  != undefined){W .classList.contains('flag') == true ? flag++ : null}
                 if(E  != undefined){E .classList.contains('flag') == true ? flag++ : null}
+                //To compare inner HTML value with number of flags we just counted - we need to make it string
                 r_flags = flag.toString();
-
+                
+                //blocking click if cell have flag
                 if (i.classList.contains('flag') == true){
                     return false;
                 }
+                //ending game if cell had a bomb
                 else if(i.classList.contains('bomb') == true){
                     i.classList.remove('untaken'); 
                     field.classList.add('gameover');
                 }
+                //Chaining that clears all cells with no number
+                else if(i.innerHTML == ''){
+                    if (FN != undefined){
+                        //testing if north-west or north-east exist
+                        if(NW != undefined){NW.classList.remove('untaken')}
+                        if(NE != undefined){NE.classList.remove('untaken')}
+                        N .classList.remove('untaken')
+                    };
+                    //Testing if south exist
+                    if (FS != undefined){
+                        //testing if south-west or south-east exist
+                        if(SW != undefined){SW.classList.remove('untaken')}
+                        if(SE != undefined){SE.classList.remove('untaken')}
+                        S .classList.remove('untaken')
+                    };
+                    //testing if west or east exist
+                    if(W  != undefined){W .classList.remove('untaken')}
+                    if(E  != undefined){E .classList.remove('untaken')}
+                    i.classList.remove('untaken'); 
+                }
+                //clicking cell that was opened
                 else if(i.classList.contains('untaken') == false){
-                    if(i.innerHTML == r_flags || i.innerHTML == ''){
+                    //cells will open only if nearby flags = number of mines
+                    if(i.innerHTML == r_flags){
+                        let mistake = 0;
+                        //Testing if north exist
                         if (FN != undefined){
-                            let NW = DigAllCells[y-1][x-1];
-                            let N  = DigAllCells[y-1][x];  
-                            let NE = DigAllCells[y-1][x+1];
-                            if(NW != undefined){NW.classList.remove('untaken')} 
-                            if(N  != undefined){N .classList.remove('untaken')} 
-                            if(NE != undefined){NE.classList.remove('untaken')}
+                            //testing if north-west or north-east exist
+                            if(NW != undefined){
+                                NW.classList.remove('untaken');
+                                NW.classList.contains('flag') == false && NW.classList.contains('bomb') == true ? mistake++ : null;
+                            }
+                            if(NE != undefined){
+                                NE.classList.remove('untaken');
+                                NE.classList.contains('flag') == false && NE.classList.contains('bomb') == true ? mistake++ : null;
+                            }
+                            N .classList.remove('untaken');
+                            N .classList.contains('flag') == false && N .classList.contains('bomb') == true ? mistake++ : null;
                         };
+                        //Testing if south exist
                         if (FS != undefined){
-                            let SW = DigAllCells[y+1][x-1];
-                            let S  = DigAllCells[y+1][x];  
-                            let SE = DigAllCells[y+1][x+1];
-                            if(SW != undefined){SW.classList.remove('untaken')}
-                            if(S  != undefined){S .classList.remove('untaken')}
-                            if(SE != undefined){SE.classList.remove('untaken')}
+                            //testing if south-west or south-east exist
+                            if(SW != undefined){
+                                SW.classList.remove('untaken');
+                                SW.classList.contains('flag') == false && SW.classList.contains('bomb') == true ? mistake++ : null;
+                            }
+                            if(SE != undefined){
+                                SE.classList.remove('untaken');
+                                SE.classList.contains('flag') == false && SE.classList.contains('bomb') == true ? mistake++ : null;
+                            }
+                            S .classList.remove('untaken');
+                            S .classList.contains('flag') == false && S .classList.contains('bomb') == true ? mistake++ : null;
                         };
-                        if(W  != undefined){W .classList.remove('untaken')}
-                        if(E  != undefined){E .classList.remove('untaken')}
+                        if(W  != undefined){
+                            W .classList.remove('untaken');
+                            W .classList.contains('flag') == false && W .classList.contains('bomb') == true ? mistake++ : null;
+                        }
+                        if(E  != undefined){
+                            E .classList.remove('untaken')
+                            E .classList.contains('flag') == false && E .classList.contains('bomb') == true ? mistake++ : null;
+                        }
+
+                        //Ending game if cell had bomb and no flag
+                        mistake>0 ? field.classList.add('gameover') : null;
                     }
                 }
+                //Removing class that hides cell
                 else {
                     i.classList.remove('untaken');     
                 }
             });
             i.addEventListener("contextmenu", function() {
+                //How flag is removed
                 if (i.classList.contains('flag') == true){
                     i.classList.remove('flag');
                     i.classList.add('untaken');
                 }
+                //Disabling option to put flag on opened cell
                 else if (i.classList.contains('untaken') == false){
                     return false; 
                 }
+                //How flag is placed
                 else {
                     i.classList.remove('untaken');
                     i.classList.add('flag'); 
